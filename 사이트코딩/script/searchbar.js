1d24accf4a067651d968c70a37e42cb33e2078a2
sesame995 <137143087+sesame995@users.noreply.github.com>
2023-07-26 14:32:14 +0900

    Add files via upload

---
+++ 사이트코딩/script/searchbar.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.btn').click(function () {
+        $('.wrap').css({ 'top': '0' });
+    });
+    $('.searchButton').click(function () {
+        $('.wrap').css({ 'top': '-58px' });
+    });
+});
